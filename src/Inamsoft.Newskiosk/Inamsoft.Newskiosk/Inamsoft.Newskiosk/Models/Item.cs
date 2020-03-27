5d199d42dd53a843ee91d5e7022cc592e163883f
Muhammed Inam <minam@apptech.de>
2020-03-27 16:10:20 +0100

    Added pull to refresh functionality

--- src/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Models/Item.cs
+++ src/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Models/Item.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="G:\git\minam365\newskiosk-mobile-app\src\Inamsoft.Newskiosk\Inamsoft.Newskiosk\Inamsoft.Newskiosk\Models\Item.cs" company="">
+//     Author:  
+//     Copyright (c) . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 
 namespace Inamsoft.Newskiosk.Models
 {
--- src/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Inamsoft.Newskiosk/ViewModels/ItemDetailViewModel.cs
+++ src/Inamsoft.Newskiosk/Inamsoft.Newskiosk/Inamsoft.Newskiosk/ViewModels/ItemDetailViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿// Copyrigth (c) 2020 Inamsoft. All rights reserved.
+//  Licensed under the GNU general public license, v3.
 
+using System;
+using System.Windows.Input;
 using Inamsoft.Newskiosk.Models;
+using Xamarin.Forms;
 
 namespace Inamsoft.Newskiosk.ViewModels
 {
     public class ItemDetailViewModel : BaseViewModel
     {
+        ICommand _refreshCommand = null;
+
         public NewsLinkItem Item { get; set; }
+
         public ItemDetailViewModel(NewsLinkItem item = null)
         {
             Title = item?.Name;
@@ -19,6 +26,28 @@ namespace Inamsoft.Newskiosk.ViewModels
         {
             get { return linkUrl; }
             set { SetProperty(ref linkUrl, value); }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                if (_refreshCommand == null)
+                {
+                    _refreshCommand = new Command(ExecuteRefreshCommand);
+                }
+
+                return _refreshCommand;
+            }
+        }
+
+        public void ExecuteRefreshCommand()
+        {
+            IsBusy = true;
+
+            OnPropertyChanged(nameof(LinkUrl));
+
+            IsBusy = false;
         }
     }
 }
